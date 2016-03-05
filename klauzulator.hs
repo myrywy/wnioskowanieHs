54f1427c584518800e984419a6723b3653e2a9a9
Marcin Lewandowski <marcinlewy22@gmail.com>
2016-03-05 10:38:06 +0100

    pierwsza wersja klauzulatora

---
+++ klauzulator.hs
@@ -0,0 +1,39 @@
+data Zdanie = 
+	Elementarne String 			-- Oznacznie zdania
+	| Rownowaznosc Zdanie Zdanie
+	| Implikacja Zdanie Zdanie
+	| Koniunkcja Zdanie Zdanie
+	| Alternatywa Zdanie Zdanie
+	| Negacja Zdanie 
+	deriving (Show)  
+
+(==>) a b = Implikacja a b
+(<=>) a b = Rownowaznosc a b
+(&&&) a b = Koniunkcja a b
+(|||) a b = Alternatywa a b
+(~~~) a = Negacja a
+
+z1 = Elementarne "z1"
+z2 = Elementarne "z2"
+z3 = Elementarne "z3"
+z4 = Elementarne "z4"
+
+i12 = z1 ==> z2
+r34 = z3 <=> z4
+
+klauzuluj::Zdanie -> Zdanie
+klauzuluj e@(Elementarne a) = e
+klauzuluj neg@(Negacja a) = neg
+klauzuluj (Implikacja a b) = klauzuluj (Alternatywa (Negacja a) b)
+klauzuluj (Rownowaznosc a b) = klauzuluj (Koniunkcja (Implikacja a b) (Implikacja b a))
+klauzuluj (Koniunkcja a b) = klauzuluj (Negacja ( Alternatywa (Negacja a) (Negacja b)) )
+klauzuluj (Alternatywa a b) = Alternatywa (klauzuluj a) (klauzuluj b)
+
+pokazStr::Zdanie -> String
+pokazStr (Rownowaznosc a b) = pokazStr a ++"<=>"++ pokazStr b
+pokazStr (Implikacja a b) = pokazStr a ++"==>"++ pokazStr b
+pokazStr (Alternatywa a b) = pokazStr a ++" | "++ pokazStr b
+pokazStr (Koniunkcja a b) = pokazStr a ++" & "++ pokazStr b
+pokazStr (Negacja a) = "~"++pokazStr a
+pokazStr (Elementarne str) = str
+
